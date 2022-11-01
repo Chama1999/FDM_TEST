3580eb38f07104f43fc181d4a81388bc474992b4
Chamath Harshana Senadeera <103487603+Chama1999@users.noreply.github.com>
2022-11-01 23:53:14 +0530

    zzz

--- predictive_web_app.py
+++ predictive_web_app.py
@@ -11,7 +11,7 @@ import streamlit as st
 
 
 # loading the saved model
-loaded_model = pickle.load(open('C:/Users/sadini/Desktop/FDM/trained_model.sav', 'rb'))
+loaded_model = pickle.load(open('C:/Users/Chamath/Desktop/Heroku/trained_model.sav', 'rb'))
 
 # creating a function for Prediction
 
